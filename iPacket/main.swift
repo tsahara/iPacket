528b3fbfcf903bfb629561a0ddfa058e25cea827
Tomoyuki Sahara <sahara@caddr.net>
2014-08-04 14:22:42 +0900

    Initial Commit

---
+++ iPacket/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  iPacket
+//
+//  Created by Tomoyuki Sahara on 8/4/14.
+//  Copyright (c) 2014 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
